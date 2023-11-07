59c7728c119d698616c8eaa721d12bacfd007d9d
antoniolopezpolo2022 <113975845+antoniolopezpolo2022@users.noreply.github.com>
2023-11-07 09:22:02 +0100

    Create suma_cifras.py

---
+++ suma_cifras.py
@@ -0,0 +1,12 @@
+def suma_cifras():
+  #Leer el número
+  suma=0
+  numero=input("Introduce un número: ")
+  #Repasar cifra a cifra y acumularlas
+  for cont in range(0,len(numero)):
+    suma=suma+int(numero[cont])
+  #Mostrar la suma
+  print("La suma vale= "+str(suma))
+
+
+suma_cifras()
